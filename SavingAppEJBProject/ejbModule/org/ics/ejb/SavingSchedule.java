afefc38a98618cfd2c205d9e5d4396743813245e
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-03-25 12:08:34 +0100

    test

--- SavingAppEJBProject/ejbModule/org/ics/ejb/SavingSchedule.java
+++ SavingAppEJBProject/ejbModule/org/ics/ejb/SavingSchedule.java
@@ -12,7 +12,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name="SavingSchedule")
 public class SavingSchedule implements Serializable{
-	
+	//testing
 	private String savingScheduleNbr;
 	private double savingGoal;
 	private int savingDurationYear;
