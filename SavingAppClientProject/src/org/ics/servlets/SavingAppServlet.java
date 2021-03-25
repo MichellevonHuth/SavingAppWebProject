2e0d44bf3fa111f35b593498d6c155149084bb0c
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-03-25 15:33:03 +0100

    works

--- SavingAppClientProject/src/org/ics/servlets/SavingAppServlet.java
+++ SavingAppClientProject/src/org/ics/servlets/SavingAppServlet.java
@@ -1,6 +1,8 @@
 package org.ics.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -14,9 +16,7 @@ import javax.servlet.http.HttpServletResponse;
 public class SavingAppServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+ 
     public SavingAppServlet() {
         super();
         // TODO Auto-generated constructor stub
@@ -26,7 +26,17 @@ public class SavingAppServlet extends HttpServlet {
 	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+
+		PrintWriter out = response.getWriter();
+		
+		out.println("<!DOCTYPE html><html><head>");
+		out.println("<title>Lab3</title>");
+		out.println("<meta charset=\"ISO-8859-1\">");
+		out.println("</head><body>");
+		out.println("<h2>Film</h2>");
+		out.println("</body></html>");
+		
+		out.close();
 	}
 
 }
