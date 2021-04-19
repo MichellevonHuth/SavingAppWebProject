aa677c2793e90343077c0d6a38f6f42385c58deb
Idacarlsson <57900909+Idacarlsson@users.noreply.github.com>
2021-04-19 16:43:14 +0200

    front end

--- SavingAppWebProject/WebContent/js/SavingApp.js
+++ SavingAppWebProject/WebContent/js/SavingApp.js
@@ -1,30 +1,3 @@
-$(document).ready(function(){
-	alert("hej");
-	
-	$("#DeleteBtn").click( function() { 
-		var strValue = $("#username").val();
-		
-		if(strValue != "") {
-			$.ajax({method: "DELETE",
-				url: "http://localhost:8080/SavingAppClientProject/"+strValue, 
-				error: ajaxFindReturnError, 
-				success: ajaxFindReturnSuccess
-			})
-		
-			
-		function ajaxDeleteReturnSuccess(result, status, xhr) {
-				clearFields();
-				$("#username").attr("placeholder", "Account has been deleted"); 
-		}
-		
-		function ajaxDeleteReturnError(result, status, xhr) {
-			alert("Error");
-			console.log("Ajax-find account: "+status);
-		}	
-	  }
-	})	
-}); 
 
-	
 	
 
--- SavingAppWebProject/src/org/ics/servlet/SavingAppServlet.java
+++ SavingAppWebProject/src/org/ics/servlet/SavingAppServlet.java
@@ -1,154 +1,364 @@
 package org.ics.servlet;
-
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.List;
-
 import javax.ejb.EJB;
 import javax.json.Json;
 import javax.json.JsonArray;
 import javax.json.JsonArrayBuilder;
+import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
+import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.ics.ejb.*;
+import org.ics.facade.*;
 
-import org.ics.ejb.Account;
-import org.ics.ejb.SavingSchedule;
-import org.ics.facade.FacadeLocal;
+
 
 /**
- * Servlet implementation class SavingAppServlet
+
+ * Servlet implementation class Movies
+
  */
-@WebServlet("/SavingAppServlet/")
+
+@WebServlet("/SavingAppServlet/*")
+
 public class SavingAppServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-  
-	@EJB 
-	FacadeLocal facade; 
- 
-	
-    public SavingAppServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+
+
+
+	@EJB
+
+	FacadeLocal facade;
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
-		String pathInfo= request.getPathInfo();
 
-		if(pathInfo== null|| pathInfo.equals("/")){
+	 * @see HttpServlet#HttpServlet()
+
+	 */
+
+	public SavingAppServlet() {
+
+		super();
+
+		// TODO Auto-generated constructor stub
+
+	}
+
+
+
+	/**
+
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+
+	 */
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+		// TODO Auto-generated method stub
+
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+
+		//response.getWriter().append("Movies ");
+
+
+/*/
+		String pathInfo = request.getPathInfo();
+
+		if(pathInfo == null || pathInfo.equals("/")){
+
 			System.out.println("Alla");
+
 			System.out.println(pathInfo);
-						
-			Account account = facade.findByAccountUsername("25");
-			sendAsJson2(response, account); 
-			
+
+
+
+			List<examples.ejb.ics.Movie> allMovies = facade.findAllMovies();
+
+			sendAsJson(response, allMovies);
+
+
+
 			return;
+
 		}
-		
+
 		String[] splits = pathInfo.split("/");
 
-		if(splits.length!= 2) {
+		if(splits.length != 2) {
+
 			System.out.println("Alla2");
+
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
 			return;
+
 		}
-		
-		//String username= splits
-		//Account account = facade.findByAccountUsername(username);
-		//sendAsJson1(response, account);
+
+		String id = splits[1];
+
+		Movie movie = facade.findByMovieId(Integer.parseInt(id));
+
+		sendAsJson(response, movie); /*/
+
 	}
 
+
+
 	/**
+
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+
 	 */
+
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		// TODO Auto-generated method stub
-		doGet(request, response);
+
+		//doGet(request, response);
+
+
+
+		String pathInfo = request.getPathInfo();
+
+		if(pathInfo == null || pathInfo.equals("/")){
+
+			BufferedReader reader = request.getReader();//Läs data Json
+
+			Account a = parseJsonAccount(reader);
+
+			try {
+
+				a = facade.createAccount(a);
+
+			}catch(Exception e) {
+
+				System.out.println("duplicate key");
+
+			}
+
+			sendAsJson(response, a);
+
+		}
+
 	}
 
+
+
 	/**
+
 	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+
 	 */
+
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
 		// TODO Auto-generated method stub
+
+		String pathInfo = request.getPathInfo();
+
+		if(pathInfo == null || pathInfo.equals("/")){
+
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
+			return;
+
+		}
+
+		String[] splits = pathInfo.split("/");
+
+		if(splits.length != 2) {
+
+			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
+			return;
+
+		}
+
+		String id = splits[1];
+
+		BufferedReader reader = request.getReader();
+
+		Account a = parseJsonAccount(reader);
+
+		//Uppdatera i db
+
+		try {
+
+			a = facade.updateAccount(a);
+
+		}catch(Exception e) {
+
+			System.out.println("facade Update Error");
+
+		}
+
+		sendAsJson(response, a);
+
 	}
+
+
 
 	/**
+
 	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+
 	 */
+
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		System.out.println("Hej");
-		String pathInfo= request.getPathInfo();
-		System.out.println(pathInfo);
-			
-		if(pathInfo== null|| pathInfo.equals("/")){
+
+		// TODO Auto-generated method stub
+
+		String pathInfo = request.getPathInfo();
+
+		if(pathInfo == null || pathInfo.equals("/")){
+
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
 			return;
+
 		}
-		
-		String[] splits= pathInfo.split("/");
-		
-		if(splits.length!= 2) {
+
+		String[] splits = pathInfo.split("/");
+
+		if(splits.length != 2) {
+
 			response.sendError(HttpServletResponse.SC_BAD_REQUEST);
+
 			return;
+
 		}
-		
-		String username = splits[1];
-		Account account = facade.findByAccountUsername(username);
-		
-		if(account!= null) {
-			facade.deleteAccount(username);
+
+		String id = splits[1];
+
+		Account account = facade.findByAccountUsername(id);
+
+		if (account != null) {
+
+			facade.deleteAccount(id);
+
 		}
-		
-		sendAsJson1(response, account);
+
+		sendAsJson(response, account);
+
+
+
 	}
-	
-	private void sendAsJson1(HttpServletResponse response, Account account) throws IOException {
-		PrintWriter out= response.getWriter();
-		response.setContentType("application/json");
-		
-		if(account!= null) {
-			out.print("{\"Username:\":");
-			out.print("\""+ account.getUsername() + "\"");
-			out.print(",\"First name\":");out.print("\""+ account.getFirstName()+"\"");
-			out.print(",\"Surname:\":");out.print("\""+ account.getSurname()+"\"}");
-		} else{	
+
+
+
+	private void sendAsJson(HttpServletResponse response, Account account) throws IOException {
+
+		PrintWriter out = response.getWriter();
+
+		response.setContentType("application/json"); 
+
+		if (account != null) {
+
+			out.print("{\"title\":");
+
+			out.print("\"" + account.getUsername() + "\"");
+
+			out.print(",\"id\":");
+
+			out.print("\"" +account.getSurname()+"\"");
+
+			out.print(",\"price\":");
+
+			out.print("\"" +account.getFirstName()+"\"}");
+
+		} else {
+
 			out.print("{ }");
-			}
-		
+
+		}
+
 		out.flush();
+
 	}
-	
-	
-	private void sendAsJson2(HttpServletResponse response, Account account) throws IOException {
-		PrintWriter out= response.getWriter();
+
+
+
+	private void sendAsJson(HttpServletResponse response, List<Account> Account) throws IOException {
+
+		/*/PrintWriter out = response.getWriter();
+
 		response.setContentType("application/json");
-		
-		if(account!= null) {
-			JsonArrayBuilder array= Json.createArrayBuilder();
 
-			for(org.ics.ejb.SavingSchedule s: account.getSavingschedules()) {
-				JsonObjectBuilder o= Json.createObjectBuilder();
-				o.add("Number", s.getSavingScheduleNbr());
-				o.add("Name", s.getSavingScheduleName());
-				o.add("Username", s.getAccount().getUsername());
+		if (movies != null) {
+
+			JsonArrayBuilder array = Json.createArrayBuilder();
+
+			for (examples.ejb.ics.Movie m : movies) {
+
+				JsonObjectBuilder o = Json.createObjectBuilder();
+
+				o.add("id", String.valueOf(m.getIdMovie()));
+
+				o.add("title", m.getTitle());
+
+				o.add("price", String.valueOf(m.getPrice()));
+
 				array.add(o);
-			}
-			
-			JsonArray jsonArray= array.build();
-			System.out.println("SavingSchedule Rest: "+jsonArray);
-			out.print(jsonArray);
-			} else{
-				out.print("[]");			
-			}
-		out.flush();} 
-	
-}
 
+			}
+
+			JsonArray jsonArray = array.build();
+
+			System.out.println("Movie Rest: "+jsonArray);
+
+			out.print(jsonArray);
+
+		} else {
+
+			out.print("[]");
+
+		}
+
+		out.flush(); /*/
+
+	}
+
+
+
+	private Account parseJsonAccount(BufferedReader br) {
+
+		//javax.json-1.0.4.jar
+
+		JsonReader jsonReader = null;
+
+		JsonObject jsonRoot = null;
+
+		
+
+		jsonReader = Json.createReader(br);
+
+		jsonRoot = jsonReader.readObject();
+
+		
+
+		System.out.println("JsonRoot: "+jsonRoot);
+
+		Account account = new Account();
+
+		
+
+		account.setUsername(jsonRoot.getString("id"));
+		account.setFirstName(jsonRoot.getString("firstName"));
+		account.setSurname(jsonRoot.getString("surname"));
+		account.setTotalIncome(Integer.parseInt(jsonRoot.getString("totalIncome")));
+		account.setVariableCost(Integer.parseInt(jsonRoot.getString("variableCost")));
+		account.setFixedCost(Integer.parseInt(jsonRoot.getString("fixedCost")));
+
+		return account;
+
+	}
+
+}
