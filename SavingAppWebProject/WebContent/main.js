eaff2f8afb3a533f5b0977151c5686b8c8b724ad
matildaforsell <56582204+matildaforsell@users.noreply.github.com>
2021-04-29 09:39:17 +0200

    Tart bort main

--- SavingAppWebProject/WebContent/main.js
+++
@@ -1,8 +0,0 @@
-function checkLogIn(){
-	var username = document.getElementById("userNameTextField").value;
-	if(username == null || username == ""){	
-		var s = "Please fill in username";
-		alert("Please fill in username");
-	}
-	return false;
-}
